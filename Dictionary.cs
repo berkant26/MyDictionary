6d4e30a5e6b804020c5142a9483d32096691a29b
berkant26 <youbeko26@gmail.com>
2021-07-06 13:34:34 +0300

    Add project files.

---
+++ Dictionary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Dictionary<S,V>
+    {
+       S[] keys;   // stringleri tutan keys dizisi 
+       V[] values;  //  degerleri(int) tutan values dizisi
+
+    public Dictionary()
+        {
+            keys = new S[0]; // burda keys ve values dizilerine bellekte adres verdik ve elemanlarini
+            values = new V[0];  //  0 olarak atadik
+
+        }
+
+        public void Add(S key, V value)
+        {
+            S[] tempKeys = keys;  // tempKeys ve tempValues , keys ve values ikinci defa new'lendigi icin eski adresteki bilgileri tutacak.
+            V[] tempValues = values;
+
+
+            keys = new S[keys.Length + 1]; // burda ikinci defa new'ledigimiz icin adresteki yer degisecek 
+            values = new V[values.Length + 1];
+
+            // bu kisimda keys ve values in eski adresteki verilerini yeni adrese aktaricaz for dongusuyle
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+            }
+            for (int i = 0; i <tempValues.Length; i++)
+            {
+                values[i] = tempValues[i];
+
+            }
+
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+            }
+        public int Lenght
+        {
+            get { return keys.Length; }
+        }
+
+
+        }
+
+
+
+
+
+    }
+
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Text;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> customer = new Dictionary<int, string>();
+            customer.Add(22,"berkant");
+            Console.WriteLine(customer.Lenght);
+
+            customer.Add(25,"berkant");
+            Console.WriteLine(customer.Lenght);
+        }
+    }
+}
